c200d9687df27e5858ae7a3cbd45cd1f74f8b434
Jaikarans <jaikrns@gmail.com>
2026-03-09 15:03:25 +0530

    fix: AboutSection content

---
+++ src/components/sections/home/AboutSectionContent.ts
@@ -0,0 +1,37 @@
+import video1 from "../../../assets/aboutSection-video-1.mp4"
+import video2 from "../../../assets/aboutSection-video-2.mp4"
+
+export const aboutSections = [
+    {
+        id: 1,
+        videoSrc: video1,
+        title: [
+            "Deep Market",
+            "Expertise,",
+            "Real-World Impact."
+        ],
+        description: [
+            "Our team brings to the table, several years of hands-on experience",
+            "across the IT landscape, enabling us to respond quickly and deliver",
+            "practical, high-value solutions.",
+            "We operate with technical excellence, agility, and a strong focus on",
+            "business results."
+        ]
+    },
+    {
+        id: 2,
+        videoSrc: video2,
+        title: [
+            "Deep Market",
+            "Expertise,",
+            "Real-World Impact."
+        ],
+        description: [
+            "Our team brings to the table, several years of hands-on experience",
+            "across the IT landscape, enabling us to respond quickly and deliver",
+            "practical, high-value solutions.",
+            "We operate with technical excellence, agility, and a strong focus on",
+            "business results."
+        ]
+    }
+];
