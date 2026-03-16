30937dfcc86007013f769917c57f78bb6203fa67
Jaikarans <jaikrns@gmail.com>
2026-03-17 00:43:31 +0530

    style: fix svg and div change color in sync with to visualize as folder

--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -70,17 +70,17 @@ const Training: React.FC = () => {
 
                 {/* SVG Flow Wave Bottom Transition */}
                 <div className="absolute bottom-0 w-full flex flex-col overflow-hidden leading-none shrink-0 pointer-events-none z-20">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1440 179" fill="none" className="hidden md:block w-full drop-shadow-sm transition-colors duration-75" style={{ color: svgFillColor, stroke: svgStrokeColor, strokeWidth: '2px' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 1440 179" fill="none" className="hidden md:block w-full drop-shadow-sm" style={{ color: svgFillColor, stroke: svgStrokeColor, strokeWidth: '2px' }}>
                         <path d="M605.979 0.190357C610.607 0.190357 615.04 2.0504 618.283 5.35031L651.587 39.2403C654.831 42.5403 659.265 44.3988 663.892 44.3988L1423.3 44.3988C1432.83 44.3988 1440.55 52.1226 1440.55 61.6498L1440.55 178.633L1436.16 178.633L0.30162 178.633L0.301634 20.1911C0.301635 9.14546 9.25598 0.191186 20.3016 0.191171L605.979 0.190357Z" fill="currentColor"></path>
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 725 118" fill="none" className="block md:hidden w-full drop-shadow-sm transition-colors duration-75" style={{ color: svgFillColor, stroke: svgStrokeColor, strokeWidth: '2px' }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 725 118" fill="none" className="block md:hidden w-full drop-shadow-sm" style={{ color: svgFillColor, stroke: svgStrokeColor, strokeWidth: '2px' }}>
                         <path d="M259.082 0.629842C263.71 0.629843 268.143 2.48988 271.386 5.7898L304.69 39.6798C307.934 42.9797 312.368 44.8382 316.995 44.8382L712.384 44.8382C719.011 44.8382 724.384 50.2109 724.384 56.8383L724.383 115.001L0.275625 117.123L0.275816 20.6306C0.275878 9.5849 9.2301 0.630655 20.2758 0.630619L259.082 0.629842Z" fill="currentColor"></path>
                     </svg>
                 </div>
             </div>
 
             {/* Courses Grid Container (Flows below wave) */}
-            <div className="-mt-0.5 grow w-full relative z-20 transition-colors duration-75" style={{ backgroundColor: svgFillColor }}>
+            <div className="-mt-0.5 grow w-full relative z-20" style={{ backgroundColor: svgFillColor }}>
                 <TrainingFeatures />
                 <div id="courses-section" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-16">
                     <div className="flex flex-col items-center justify-center w-full mb-16">
