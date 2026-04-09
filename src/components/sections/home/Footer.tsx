c35a5ba59719102fbfa47143caac105ffbed8708
Jaikarans <jaikrns@gmail.com>
2026-04-09 16:08:07 +0530

    remove unused var

--- src/components/sections/home/Footer.tsx
+++ src/components/sections/home/Footer.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useLenis } from 'lenis/react'
-import { FaLinkedinIn, FaFacebookF, FaInstagram, FaMapPin } from 'react-icons/fa'
+import { FaLinkedinIn, FaFacebookF, FaInstagram } from 'react-icons/fa'
 import logoFooter from '../../../assets/logo-footer.svg'
 import { FaLocationDot } from 'react-icons/fa6'
-
-const PinIcon = () => (
-  <svg className="shrink-0 mt-0.5 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7Z" />
-    <circle cx="12" cy="9" r="2.5" />
-  </svg>
-)
 
 
 const capabilities = [
